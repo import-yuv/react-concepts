5c7e81e78e3fcab99b972277b79e49a726e78116
Rakesh <rakesh@smilebots.com>
2021-03-21 11:28:33 +0530

    React-Clone ELemet exmaple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <h2>Parent</h2>
+          <Hello>
+            <button value="Button A">A</button>
+            <button value="Button B">B</button>
+            <button value="Button C">C</button>
+          </Hello>
+        </div>
+      </>
+    );
+  }
 }
 
-export default App;
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedButton: "None",
+    };
+  }
+  selectedButton = (selectedButton) => {
+    this.setState({ selectedButton });
+  };
+  render() {
+    let newfunction = (child) =>
+      React.cloneElement(child, {
+        onClick: this.selectedButton.bind(this, child.props.value),
+      });
+
+    let items = React.Children.map(this.props.children, newfunction);
+
+    return (
+      <>
+        <h2>Children</h2>
+
+        <h2>You Select:{this.state.selectedButton}</h2>
+        {items}
+      </>
+    );
+  }
+}
+export default Parent;
